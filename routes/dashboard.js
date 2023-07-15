3a8474627d63be558c1436505c50cbe6459c03f9
rahul-mobiledev <rahulmobiledeveloper@gmail.com>
2023-07-15 17:26:10 +0530

    Dashboard API

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { db } = require("../db/conn.js");
-const {datesAreOnSameDay} = require("../utils/time.js");
+const {datesAreOnSameDay, datesAreOnSameMonth,getMonthInYear,getDaysInMonth} = require("../utils/time.js");
 
 const router = express.Router();
 
@@ -10,19 +10,33 @@ router.post("/", async (req, res) => {
   let orders = await collection.find({}).toArray();
   let response = {
     todayCollection : {
-      amount : getTodaysCollection(orders,request.time),
+      amount : getCollection(orders,new Date(request.date),false),
       goal : 10000,
-      orders : getTodaysOrders(orders,request.time)
-    }
+      orders : getOrders(orders,new Date(request.date),false)
+    },
+    monthCollection : {
+      amount : getCollection(orders,new Date(request.date),true),
+      goal : 250000,
+      orders : getOrders(orders,new Date(request.date),true)
+    },
+    monthFilterCollection : getMonthCollectionDaily(orders,request.monthlyCollectionDate),
+    yearFilterCollection : getYearCollectionMonthly(orders,request.yearlyCollectionDate)
   }
   res.send(response).status(200);
 });
 
-function getTodaysCollection(orders,date){
+function getCollection(orders,date,isMonthDate){
   let collection = 0;
   orders.forEach(order => {
     order.payments.forEach(payment => {
-      let isTodayPayment = datesAreOnSameDay(new Date(date),new Date(payment.time));
+      let isTodayPayment;
+      if(isMonthDate){
+        isTodayPayment = datesAreOnSameMonth(date,new Date(payment.time));
+      }
+      else{
+        isTodayPayment = datesAreOnSameDay(date,new Date(payment.time));
+      }
+      
       if(isTodayPayment){
         collection += payment.amount;
       }
@@ -31,17 +45,51 @@ function getTodaysCollection(orders,date){
   return collection;
 }
 
-function getTodaysOrders(orders,date){
+function getOrders(orders,date,isMonthDate){
   let totalOrder = 0;
   orders.forEach(order => {
-    let currentStatus = order.statusHistory[order.statusHistory.length -1];
-    let isTodayOrder = datesAreOnSameDay(new Date(date),new Date(currentStatus.time));
-
-    if(currentStatus.status === "active" && isTodayOrder){
+    let isTodayOrder = true;
+    let currentIndex = order.statusHistory.length
+    while(currentIndex > 0){
+      let status = order.statusHistory[currentIndex-1];
+      let matched;
+      if(isMonthDate){
+        matched = datesAreOnSameMonth(date,new Date(status.time))
+      }
+      else{
+        matched = datesAreOnSameDay(date,new Date(status.time))
+      }
+      if(matched){
+        currentIndex--;
+      }
+      else{
+        isTodayOrder = false;
+        break;
+      }
+    }
+    if(isTodayOrder){
       totalOrder++;
     }
   });
   return totalOrder;
+}
+
+function getMonthCollectionDaily(orders,date){
+  return getDaysInMonth(new Date(date).getMonth() , new Date(date).getFullYear()).map((e)=>{
+    return {
+      dayMonth : e.getDate(),
+      amount : getCollection(orders,e,false)
+    }
+  })
+}
+
+function getYearCollectionMonthly(orders,date){
+  return getMonthInYear(new Date(date).getFullYear()).map((e)=>{
+    return {
+      dayMonth : e.getMonth()+1,
+      amount : getCollection(orders,e,true)
+    }
+  })
 }
 
 module.exports = router;
--- utils/time.js
+++ utils/time.js
@@ -7,8 +7,31 @@ const datesAreOnSameMonth = (first, second) =>
     first.getFullYear() === second.getFullYear() &&
     first.getMonth() === second.getMonth();
 
+
+const getDaysInMonth = (month, year) => {
+    var date = new Date(year, month, 1);
+    var days = [];
+    while (date.getMonth() === month) {
+        days.push(new Date(date));
+        date.setDate(date.getDate() + 1);
+    }
+    return days;
+}
+
+const getMonthInYear = (year) => {
+    var date = new Date(year, 0);
+    var months = [];
+    while (date.getFullYear() === year) {
+        months.push(new Date(date));
+        date.setMonth(date.getMonth() + 1);
+    }
+    return months;
+}
+
 module.exports = {
     datesAreOnSameDay: datesAreOnSameDay,
-    datesAreOnSameMonth : datesAreOnSameMonth
+    datesAreOnSameMonth: datesAreOnSameMonth,
+    getDaysInMonth : getDaysInMonth,
+    getMonthInYear : getMonthInYear
 }
 
