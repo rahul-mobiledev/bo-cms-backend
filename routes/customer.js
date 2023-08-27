4302612abf50c245c36d89ddb15c57a33c6308dd
rahul-mobiledev <rahulmobiledeveloper@gmail.com>
2023-08-27 12:45:41 +0530

    Update Customer.js

--- routes/customer.js
+++ routes/customer.js
@@ -6,19 +6,31 @@ const router = express.Router();
 // Get a list of 50 posts
 router.get("/", async (req, res) => {
   let words = req.query.search.toLowerCase();
+  let regexText = new RegExp(words);
   let collection = db(req).collection("customer");
-  let results = await collection.find({}).toArray();
-  results = results.filter((element) => {
-    return element.name.toLowerCase().includes(words) || element.no.includes(words);
-  })
+  let results = await collection.find({
+    $or : [
+      {
+        no : {
+          $regex : regexText
+        }
+      },
+      {
+        name : {
+          $regex : regexText
+        }
+      }
+    ]
+  }).toArray();
+
   res.send(results.reverse()).status(200);
 });
 
 router.get("/id", async (req, res) => {
-  let id = req.query.id.toLowerCase();
+  let id = req.query.id;
   let collection = db(req).collection("customer");
   let results = await collection.findOne({
-    _id : id
+    _id: id
   });
   res.send(results).status(200);
 });
